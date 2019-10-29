177c8877a111352b800cd0da730c5341fb838db8
Lomash Sharma <lomashss09@gmail.com>
2019-10-29 20:51:10 +0530

    final commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app = express();
 
 const { Client } = require('pg')
 
+// temp database
 const client = new Client({
   user:"postgres",
   password:"test123",
@@ -19,9 +20,6 @@ const client = new Client({
 client.connect()
 .then(()=>console.log('connected successfully'))
 .catch(e=>console.log)
-// .finally(()=>client.end())
-
-// Assign Dust Engine to .dust files
 app.engine('dust',cons.dust);
 
 // Set Default Ext .dust
@@ -41,13 +39,11 @@ console.log(result.rows)
   res.render('index',{item:result.rows})
     });
 app.post('/add',async(req,res)=>{
-  console.log(req.body)
   var item = req.body.newitem
   await client.query("INSERT INTO item(items) VALUES ($1)",[item])
   res.redirect('/');
 })
 app.delete('/delete',async(req,res)=>{
-console.log(req.body)
 })
 app.post('/delete',async(req,res)=>{
   result = Object.keys(req.body)[0]
@@ -55,4 +51,4 @@ app.post('/delete',async(req,res)=>{
   res.redirect('/')
 })
 
-app.listen(3089,()=>console.log('successfully running on port'));
+app.listen(3091,()=>console.log('successfully running on port'));
