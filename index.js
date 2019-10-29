92fb173451d0f6dec2eb4213fe9479873245e4e8
Lomash Sharma <lomashss09@gmail.com>
2019-10-29 19:15:42 +0530

    complete basic functionality

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ path = require('path'),
 bodyParser =  require('body-parser'),
 cons = require('consolidate'),
 dust = require('dustjs-helpers'),
-pg = require('pg')
+
 app = express();
 
-// DB Connect String
-var connect = "postgres://lomash:test123@localhost/tododb";
+
+const { Client } = require('pg')
+
+const client = new Client({
+  user:"postgres",
+  password:"test123",
+  host:"localhost",
+  port:5432,
+  database:"tododb"
+})
+client.connect()
+.then(()=>console.log('connected successfully'))
+.catch(e=>console.log)
+// .finally(()=>client.end())
 
 // Assign Dust Engine to .dust files
 app.engine('dust',cons.dust);
@@ -23,13 +35,24 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/',(req,res)=>{
-    pg.connect(conString, (err,client,done){
-        if(err){
-            return console.error('error fetching client from pool',err)
-        }
-        client.query('SELECT ',['1'],(err,result))
-    })
-});
+app.get('/',async(req, res)=>{
+var result = await client.query('SELECT * FROM item')
+console.log(result.rows)
+  res.render('index',{item:result.rows})
+    });
+app.post('/add',async(req,res)=>{
+  console.log(req.body)
+  var item = req.body.newitem
+  await client.query("INSERT INTO item(items) VALUES ($1)",[item])
+  res.redirect('/');
+})
+app.delete('/delete',async(req,res)=>{
+console.log(req.body)
+})
+app.post('/delete',async(req,res)=>{
+  result = Object.keys(req.body)[0]
+  await client.query(`DELETE FROM item WHERE id = ${result}`)
+  res.redirect('/')
+})
 
-app.listen(3008,()=>console.log('successfully running on port'));
+app.listen(3089,()=>console.log('successfully running on port'));
